4fceb1468707e455a58778bfc7a2b3dc3f8fb1ac
Ammar UD Din <ammarreloaded@gmail.com>
2022-10-02 16:04:25 +0500

    Initial commit

---
+++ app/src/test/java/com/example/cusit_11637_s5_se_asgnmnt_2_scientific_calculator/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.cusit_11637_s5_se_asgnmnt_2_scientific_calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
